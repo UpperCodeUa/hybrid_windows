a67f13c66682a95493c2ff89f21cef08cbca4746
dmitriy.thorzhevskii@gmail.com <dmitriy.thorzhevskii@gmail.com>
2025-10-18 20:24:15 +0300

    add kommo lead

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,3 +1,7 @@
+require "net/http"
+require "uri"
+require "json"
+
 class HomeController < ApplicationController
   before_action :build_request
 
@@ -19,13 +23,16 @@ class HomeController < ApplicationController
   def create_request
     unless verify_hcaptcha(params["g-recaptcha-response"])
       return redirect_back fallback_location: contacts_path,
-                           alert:  I18n.t("confirmation.fail_request")
+                           alert: I18n.t("confirmation.fail_request")
     end
 
     @request = Request.new(request_params)
 
     if @request.save
       TelegramNotificationService.new(params).call if Rails.env.production?
+
+      create_kommo_lead(@request)
+
       redirect_to thank_you_request_path, notice: I18n.t("confirmation.success_request")
     else
       redirect_back fallback_location: contacts_path, alert: I18n.t("confirmation.fail_request")
@@ -53,5 +60,60 @@ class HomeController < ApplicationController
     result = JSON.parse(http_response.body)
     Rails.logger.info "hCaptcha verification result: #{result}"
     result["success"]
+  end
+
+  # === Kommo CRM integration ===
+  def create_kommo_lead(request)
+    domain = "perfectgroupcrm.kommo.com"
+    token = "Ogqwh9QXWwtLZPvrJVhwbqxquCf2vWbGt7nGmFAAJGB6VkD72XxpEYDwEkxH5sS1"    
+
+    contact_data = [
+      {
+        name: request.name,
+        custom_fields_values: [
+          {
+            field_code: "PHONE",
+            values: [{ value: request.phone }]
+          }
+        ]
+      }
+    ]
+
+    uri_contacts = URI("https://#{domain}/api/v4/contacts")
+    contact_response = http_post(uri_contacts, contact_data, token)
+    contact_id = contact_response.dig("_embedded", "contacts", 0, "id")
+
+    lead_data = [
+      {
+        name: "Заявка с сайта",
+        price: 0,
+        _embedded: {
+          contacts: contact_id ? [{ id: contact_id }] : []
+        }
+#        custom_fields_values: [
+#          {
+#            field_name: "Комментарий",
+#            values: [{ value: request.message }]
+#         }
+#        ]
+      }
+    ]
+
+    uri_leads = URI("https://#{domain}/api/v4/leads")
+    http_post(uri_leads, lead_data, token)
+  rescue => e
+    Rails.logger.error "Ошибка при создании лида в Kommo: #{e.message}"
+  end
+
+  def http_post(uri, data, token)
+    http = Net::HTTP.new(uri.host, uri.port)
+    http.use_ssl = true
+    req = Net::HTTP::Post.new(uri.path, {
+      "Authorization" => "Bearer #{token}",
+      "Content-Type" => "application/json"
+    })
+    req.body = data.to_json
+    res = http.request(req)
+    JSON.parse(res.body)
   end
 end
